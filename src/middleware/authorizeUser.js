bbf9b63262fe5fa9d9ec191b97acd53d461b7c15
LT-Schmiddy <schmidalex1996@gmail.com>
2026-04-16 13:00:52 -0400

    more robust user authorization

--- src/middleware/authorizeUser.js
+++ src/middleware/authorizeUser.js
@@ -1,5 +1,70 @@
 // import { getPostById } from "../services/postService.js";
 
+// A more robust authorization mechanism using identifier strings.
+// Valid strings are:
+// * role:{role} - the logged in user has this role. Example: 'role:ADMIN'
+// * user:{id} - the logged in user is a specific user. Example: 'user:6'
+// * user:me - the logged in user is matches the `user_id` parameter. Example: 'user:me'
+export function authorizeUserAccessRules(...ruleStrings) {    
+    return function (req, res, next) {
+        // Making the error handling a function here for convenience.
+        function throw_invalid_access_rule(str, explanation) {
+            const err = new Error(`Internal Server Error: Invalid access rule ${str} - ${explanation}`);
+            err.status = 500;
+            err.log_only = true;
+            return next(err);
+        }
+
+        let approved = false;
+
+        ruleStrings.forEach(
+            /**
+             * @param {string} rule
+             * @param {number} index
+             */
+            (rule) => {
+                if (typeof rule === "string") {
+                    if (!rule.includes(":")) {
+                        throw_invalid_access_rule(rule, "the ':' seperator is missing");
+                    }
+
+                    let str_args = rule.split(":");
+                    let str_type = str_args[0];
+                    let str_val = str_args[1];
+
+                    switch (str_type) {
+                        case 'role':
+                            // This is an approved role:
+                            approved ||= req.user.role === str_val;
+                            break;
+                        case 'user':
+                            if (str_val === 'me') {
+                                // The logged in user is the user from user_id
+                                approved ||= req.user.id === req.param_user_id;
+                            } else {
+                                // The logged in user is this specific user.
+                                approved ||= req.user.id === parseInt(str_val);
+                            }
+                            break;
+                        default:
+                            throw_invalid_access_rule(rule, `invalid rule string type '${str_type}'`);
+                    }
+                }
+                else {
+                    throw_invalid_access_rule(rule, `invalid rule type '${typeof rule}'`);
+                }                
+            }
+        );
+
+        if (!approved) {
+            const err = new Error('Forbidden: insufficient permission');
+            err.status = 403;
+            return next(err);
+        }
+        return next();
+    };
+}
+
 export async function authorizeOwnership(req, res, next) {
     const id = parseInt(req.user.id);
     // const post = await getPostById(id);
@@ -10,16 +75,4 @@ export async function authorizeOwnership(req, res, next) {
     //     return next(err);
     // }
     next();
-}
-
-
-export function authorizeRoles(...allowedRoles) {
-    return function (req, res, next) {
-        if (!allowedRoles.includes(req.user.role)) {
-            const err = new Error('Forbidden: insufficient permission');
-            err.status = 403;
-            return next(err);
-        }
-        return next();
-    };
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,14 +8,13 @@ import {
 import { authenticate } from '../middleware/authenticate.js';
 import { validateUserIdParam, handleUserParamIsMe } from '../middleware/validateUserIdParam.js'
 import { validateUpdateUser } from '../middleware/userValidators.js';
-import { authorizeRoles, authorizeOwnership } from '../middleware/authorizeUser.js';
+import { authorizeUserAccessRules } from '../middleware/authorizeUser.js';
 const router = express.Router();
 
-router.get('/', authenticate, authorizeRoles('ADMIN'), getAllUsersHandler);
-router.get('/:user_id', authenticate, validateUserIdParam, handleUserParamIsMe, getLoggedInUserHandler);
-router.put('/:user_id', authenticate, validateUserIdParam, handleUserParamIsMe, validateUpdateUser, updateLoggedInUserHandler);
-router.delete('/:user_id', authenticate, validateUserIdParam, handleUserParamIsMe, deleteLoggedInUserHandler);
-// router.get('/me/posts', authenticate, getPostsForLoggedInUserHandler);
+router.get('/', authenticate, authorizeUserAccessRules("role:ADMIN"), getAllUsersHandler);
+router.get('/:user_id', authenticate, validateUserIdParam, handleUserParamIsMe, authorizeUserAccessRules("role:ADMIN", "user:me"), getLoggedInUserHandler);
+router.put('/:user_id', authenticate, validateUserIdParam, handleUserParamIsMe, authorizeUserAccessRules("role:ADMIN", "user:me"), validateUpdateUser, updateLoggedInUserHandler);
+router.delete('/:user_id', authenticate, validateUserIdParam, handleUserParamIsMe, authorizeUserAccessRules("role:ADMIN", "user:me"), deleteLoggedInUserHandler);
 router.get('/:user_id/patch', authenticate, validateUserIdParam, handleUserParamIsMe, (req, res) => {
     console.log('poop');
 });
