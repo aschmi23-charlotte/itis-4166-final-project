577d2ca386166c22c03041f4977893cdd85f158d
LT-Schmiddy <schmidalex1996@gmail.com>
2026-04-25 17:27:17 -0400

    removing deadline from prisma schema for now

---
+++ prisma/migrations/20260425212633_removing_deadline_from_listitem/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `deadline` on the `todolistitem` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "todolistitem" DROP COLUMN "deadline";
